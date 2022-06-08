e8ac43f55fb82c476fef873e7ee651fc2227793a
cenelif@gmail.com <cenelif@gmail.com>
2022-06-08 18:49:50 +0300

    Add project files.

---
+++ ETicaret2022/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ETicaret2022.Models;
+
+namespace ETicaret2022.Controllers
+{
+    public class HomeController : Controller
+    {
+        ETicaret2022Entities db = new ETicaret2022Entities();
+        public ActionResult Index()
+        {
+            ViewBag.Kategoriler = db.Kategori.ToList();
+            ViewBag.Urunler = db.Urunler.OrderByDescending(x => x.UrunID).Take(10).ToList();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ETicaret2022/Controllers/KategoriController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ETicaret2022.Models;
+
+namespace ETicaret2022.Controllers
+{
+    public class KategoriController : Controller
+    {
+        private ETicaret2022Entities db = new ETicaret2022Entities();
+
+        // GET: Kategori
+        public ActionResult Index()
+        {
+            return View(db.Kategori.ToList());
+        }
+
+        // GET: Kategori/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategori kategori = db.Kategori.Find(id);
+            if (kategori == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategori);
+        }
+
+        // GET: Kategori/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+     
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "KategoriID,KategoriAdi")] Kategori kategori)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Kategori.Add(kategori);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(kategori);
+        }
+
+        // GET: Kategori/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategori kategori = db.Kategori.Find(id);
+            if (kategori == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategori);
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "KategoriID,KategoriAdi")] Kategori kategori)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(kategori).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kategori);
+        }
+
+        // GET: Kategori/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategori kategori = db.Kategori.Find(id);
+            if (kategori == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategori);
+        }
+
+        // POST: Kategori/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Kategori kategori = db.Kategori.Find(id);
+            db.Kategori.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
