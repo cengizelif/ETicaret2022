bea92f876f4996d830c32da55ad5e6e97fd98747
cenelif@gmail.com <cenelif@gmail.com>
2022-06-09 17:49:57 +0300

    Sepet Controller

--- ETicaret2022/Controllers/SepetController.cs
+++ ETicaret2022/Controllers/SepetController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -17,6 +18,7 @@ namespace ETicaret2022.Controllers
             Urunler urun = db.Urunler.Find(id);
 
             Sepet sepettekiurun = db.Sepet.FirstOrDefault(x => x.UrunID == id);
+
 
             string userID = User.Identity.GetUserId();
 
@@ -36,6 +38,48 @@ namespace ETicaret2022.Controllers
                 sepettekiurun.Adet = sepettekiurun.Adet + (adet??1);
                 sepettekiurun.ToplamTutar = sepettekiurun.Adet * urun.UrunFiyati;
             }
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    
+        public ActionResult Index()
+        {
+            string userID = User.Identity.GetUserId();
+           
+            var sepet = db.Sepet.Where(x => x.UserID == userID).Include(u => u.Urunler);
+
+            return View(sepet.ToList());
+        }
+
+        public ActionResult SepetGuncelle(int? adet,int? id)
+        {
+            if(id==null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            Sepet sepet = db.Sepet.Find(id);
+
+            if(sepet==null)
+            {
+                return HttpNotFound();
+            }
+
+            Urunler urun = db.Urunler.Find(sepet.UrunID);
+
+            sepet.Adet = adet ?? 1;
+            sepet.ToplamTutar = sepet.Adet * urun.UrunFiyati;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult Sil(int id)
+        {
+            Sepet sepet = db.Sepet.Find(id);
+            db.Sepet.Remove(sepet);
             db.SaveChanges();
 
             return RedirectToAction("Index");
---
+++ ETicaret2022/Controllers/SiparisController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ETicaret2022.Models;
+
+namespace ETicaret2022.Controllers
+{
+    public class SiparisController : Controller
+    {
+        ETicaret2022Entities db = new ETicaret2022Entities();
+        public ActionResult Index()
+        {                       
+            return View(db.Siparis.ToList());
+        }
+    }
+}
