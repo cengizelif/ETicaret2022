1987aae5d401feb12158112ac73c8750823a0421
cenelif@gmail.com <cenelif@gmail.com>
2022-06-20 19:47:46 +0300

    WebApi ile Kategori listelendi

--- ETicaret2022/Controllers/HomeController.cs
+++ ETicaret2022/Controllers/HomeController.cs
@@ -1,18 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using ETicaret2022.Models;
+
+using Newtonsoft.Json;
 
 namespace ETicaret2022.Controllers
 {
     public class HomeController : Controller
     {
         ETicaret2022Entities db = new ETicaret2022Entities();
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            ViewBag.Kategoriler = db.Kategori.ToList();
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44390/");
+
+            var result = await client.GetAsync("api/Kategoriler");
+
+            var sonuc = await result.Content.ReadAsStringAsync();
+
+            ViewBag.Kategoriler = JsonConvert.DeserializeObject<List<Kategori>>(sonuc);
+
+            //ViewBag.Kategoriler = db.Kategori.ToList();
             ViewBag.Urunler = db.Urunler.OrderByDescending(x => x.UrunID).Take(10).ToList();
 
             return View();
@@ -23,8 +36,8 @@ namespace ETicaret2022.Controllers
             Kategori kat=db.Kategori.Find(id);
 
             ViewBag.KategoriAd = kat.KategoriAdi;
-
             ViewBag.Kategoriler = db.Kategori.ToList();
+          
 
             return View(db.Urunler.Where(x=>x.KategoriID==id).OrderBy(x=>x.UrunAdi).ToList());
         }
---
+++ WebAPIProject/Controllers/KategorilerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIProject.Models;
+
+namespace WebAPIProject.Controllers
+{
+    public class KategorilerController : ApiController
+    {
+        public List<Kategorim> Get()
+        {
+            ETicaret2022Entities db = new ETicaret2022Entities();
+
+            //List<Kategori> kategoriler = (from x in db.Kategori
+            //                              select new Kategori { KategoriID = x.KategoriID, KategoriAdi = x.KategoriAdi }).ToList();
+
+            List<Kategori> kategoriler = db.Kategori.ToList();
+
+            List<Kategorim> liste = new List<Kategorim>();
+
+            foreach (var item in kategoriler)
+            {
+                liste.Add(new Kategorim() { KategoriID = item.KategoriID, KategoriAdi = item.KategoriAdi }) ;
+            }
+
+            return liste;
+        }
+    }
+}
